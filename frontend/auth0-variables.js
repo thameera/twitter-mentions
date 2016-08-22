6fb78a9e0d0eae7a46eaaa83d0adab308b2139df
Thameera Senanayaka <thameera123@gmail.com>
2016-08-22 19:37:33 +0530

    Basic login with twitter

---
+++ frontend/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='UFMZQmUIDIM9m83HnaODOF7BqqJfLxBF';
+var AUTH0_DOMAIN='tham-projects.auth0.com';
+var AUTH0_CALLBACK_URL=location.href;
---
+++ frontend/script.js
@@ -0,0 +1,49 @@
+$(function() {
+
+  var auth0 = null;
+  auth0 = new Auth0({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    callbackOnLocationHash: true,
+    callbackURL: AUTH0_CALLBACK_URL
+  });
+
+  var authorize = function() {
+    auth0.login({
+      connection: 'twitter'
+    }, function(err) {
+      if (err) console.log("something went wrong: " + err.message);
+    });
+  };
+
+  var show_logged_in = function() {
+    $('#login').hide();
+    $('#authorized').show();
+  };
+
+  var show_sign_in = function() {
+    $('#login').show();
+    $('#authorized').hide();
+  };
+
+  var parseHash = function() {
+    var token = localStorage.getItem('id_token');
+    if (null != token) {
+      show_logged_in();
+    } else {
+      var result = auth0.parseHash(window.location.hash);
+      if (result && result.idToken) {
+        localStorage.setItem('id_token', result.idToken);
+        show_logged_in();
+      } else if (result && result.error) {
+        console.log('error: ' + result.error);
+        show_sign_in();
+      }
+    }
+  };
+
+  $('#authorize').click(authorize);
+
+  parseHash();
+
+});
