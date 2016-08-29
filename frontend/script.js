e23fdcd0be38efa7dd7bd2f0916edaa2e8a58044
Thameera Senanayaka <thameera123@gmail.com>
2016-08-30 00:31:06 +0530

    Call webtask from webapp

--- frontend/script.js
+++ frontend/script.js
@@ -21,9 +21,10 @@ $(function() {
     window.location.href = '/';
   };
 
-  var show_logged_in = function() {
+  var show_logged_in = function(idToken) {
     $('#login').hide();
     $('#authorized').show();
+    getMentions(idToken);
   };
 
   var show_sign_in = function() {
@@ -31,15 +32,29 @@ $(function() {
     $('#authorized').hide();
   };
 
+  var getMentions = function(idToken) {
+    var url = 'https://webtask.it.auth0.com/api/run/wt-thameera-auth0_com-0/get-twitter-mentions?webtask_no_cache=1';
+    $.post(url, {
+      token: idToken
+    })
+      .done(function(res) {
+        console.log(res);
+      })
+      .fail(function(err) {
+        console.log('err');
+        console.error(err);
+      });
+  };
+
   var parseHash = function() {
     var token = localStorage.getItem('id_token');
     if (null != token) {
-      show_logged_in();
+      show_logged_in(token);
     } else {
       var result = auth0.parseHash(window.location.hash);
       if (result && result.idToken) {
         localStorage.setItem('id_token', result.idToken);
-        show_logged_in();
+        show_logged_in(result.idToken);
       } else if (result && result.error) {
         console.log('error: ' + result.error);
         show_sign_in();
